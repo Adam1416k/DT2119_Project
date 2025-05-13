fb8576bb0036444931c76d1a7bd856d2c0c9e514
Oscar Tegnér Mohringe <58402563+oscar-tm@users.noreply.github.com>
2025-05-13 12:27:41 +0200

    Added rttm loading capabilities
    New function to load and evaluate from paths #15

--- diarization/evaluator.py
+++ diarization/evaluator.py
@@ -17,3 +17,16 @@ def compute_metrics(reference, hypothesis):
     Conf = components["confusion"]
 
     return DER, FA, Miss, Conf
+
+
+def load_and_compute_metrics(ref_path, hyp_path):
+    """
+    Load the requested RTTM files and computes the metrics.
+    """
+
+    ref_rttm = load_rttm(ref_path)
+    hyp_rttm = load_rttm(hyp_path)
+
+    # Should only be one key that is the same for both files, and the key should be the file id
+    file_id = list(ref_rttm.keys())[0]
+    return compute_metrics(ref_rttm[file_id], hyp_rttm[file_id])
